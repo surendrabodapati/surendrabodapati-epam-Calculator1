4d4364ead93b09578693aeb2f032429124e11e25
surendrabodapati <52579540+surendrabodapati@users.noreply.github.com>
2020-02-10 23:51:29 +0530

    Add files via upload

---
+++ input/Read.java
@@ -0,0 +1,19 @@
+package input;
+
+import java.util.Scanner;
+
+public class Read {
+	public int a,b;
+	public char ch;
+	public Read()
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter first number");
+		a=sc.nextInt();
+		System.out.println("Enter second number");
+		b=sc.nextInt();
+		System.out.println("Please Enter operator");
+		ch=sc.next().charAt(0);
+	}
+
+}
