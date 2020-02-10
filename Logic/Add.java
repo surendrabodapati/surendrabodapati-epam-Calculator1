34cd2494c83365de4d572a1484d4e775b5dfec4d
surendrabodapati <52579540+surendrabodapati@users.noreply.github.com>
2020-02-11 00:36:19 +0530

    Update Add.java

--- Logic/Add.java
+++ Logic/Add.java
@@ -3,7 +3,7 @@ package Logic;
 public class Add {
 	public Add(double a, double b)
 	{
-		System.out.println("Addition of "+a+" and "+b+" is : "+a/b);
+		System.out.println("Addition of "+a+" and "+b+" is : "+(a+b));
 	}
 
 }
